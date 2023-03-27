06d09fc3291f490c5ba13771e1603de39b2719b4
Snowy <100844814+snowypark@users.noreply.github.com>
2023-03-27 12:46:50 +0900

    Add files via upload

---
+++ 03-20 STACK/Linked.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent; // ConcurrentStack
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new ConcurrentStack<int>();
+
+            // 데이타를 스택에 넣는 쓰레드
+            Task tPush = Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    s.Push(i);
+                    Thread.Sleep(100);
+                }
+            });
+
+            // 데이타를 스택에서 읽는 쓰레드
+            Task tPop = Task.Factory.StartNew(() =>
+            {
+                int n = 0;
+                int result;
+                while (n < 100)
+                {
+                    if (s.TryPop(out result))
+                    {
+                        Console.WriteLine(result);
+                        n++;
+                    }
+                    Thread.Sleep(150);
+                }
+            });
+
+            // 두 쓰레드가 끝날 때까지 대기
+            Task.WaitAll(tPush, tPop);
+        }
+
+
+    }
+
+
+}
